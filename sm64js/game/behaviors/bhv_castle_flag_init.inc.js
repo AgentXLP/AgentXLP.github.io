20a44969222e5667c5f3ff12034e3fbe84069449
Agent 11 <techgod18@outlook.com>
2021-03-21 14:41:32 -0400

    Add sm64js
    you can now play my fork (hopefully)

--- sm64js/game/behaviors/bhv_castle_flag_init.inc.js
+++
@@ -1,6 +0,0 @@
-import { ObjectListProcessorInstance as ObjectListProc } from "../ObjectListProcessor"
-
-export const bhv_castle_flag_init = () => {
-    const o = ObjectListProc.gCurrentObject
-    o.header.gfx.unk38.animFrame = Math.floor(Math.random() * 28.0)
-}
--- sm64js/include/config.js
+++
@@ -1,2 +0,0 @@
-export const SCREEN_WIDTH = 320
-export const SCREEN_HEIGHT = 240
