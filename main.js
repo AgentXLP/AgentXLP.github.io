2e2fd5584a9fc44cf56f8fd6796e160eff0f58e1
Agent-11 <techgod18@outlook.com>
2021-08-08 10:12:30 -0400

    Landing page update

--- main.js
+++ main.js
@@ -29,31 +29,36 @@ window.addEventListener('resize', () =>
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const ambientLight = new THREE.AmbientLight(0xc8aeeb)
+const ambientLight = new THREE.AmbientLight(0x38423a)
 scene.add(ambientLight)
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 2)
+scene.add(dirLight)
 
 const bg = new THREE.TextureLoader().load('bg.png')
 scene.background = bg
 
 // Avatar
 
-const lolTex = new THREE.TextureLoader().load('/assets/lol.png')
+const tex = new THREE.TextureLoader().load('/assets/img/this_image_is_legal_because_it_is_from_the_render96_pack.png')
 
-const lol = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshPhongMaterial({ map: lolTex }))
+window.sphere = {
+  speed: 0.01,
+  zRot: 180,
+  xPos: 2
+}
 
-scene.add(lol)
+const geo = new THREE.SphereGeometry(2, 32, 16)
+const object = new THREE.Mesh(geo, new THREE.MeshStandardMaterial({ map: tex }))
+object.position.x = window.sphere.xPos
+object.rotation.z = window.sphere.zRot
+
+scene.add(object)
 
 function animate() {
   requestAnimationFrame(animate)
-  lol.rotation.y += 0.01
+  object.rotation.y += window.sphere.speed
 
   renderer.render(scene, camera)
 }
 animate()
-
-let mus = new Audio('./assets/aud/smwremix.mp3')
-  mus.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
-    this.currentTime = 0
-    this.play()
-  }, false)
-  mus.play()
