6b205423abc442bfbb8350be28c27cd40799d800
Agent-11 <techgod18@outlook.com>
2021-09-17 07:08:54 -0400

    Le epic unblockers base
    totally not a namejeff.xyz rip off

---
+++ projects/ubs/index.js
@@ -0,0 +1,3 @@
+console.log('%cWhat are you doing here 😳😳😳😳', 'font-size: 40px;') // the funny
+console.log("Also in case you haven't noticed I'm trying to make this look like https://namejeff.xyz lol")
+document.title = document.getElementById("topText").textContent
