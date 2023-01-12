d1b024f2f033bf5f9539115a4ff09f0cf04c9433
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2023-01-11 22:29:01 -0500

    I should relearn web development

--- copyright-gen/copyright-gen.js
+++
@@ -1,7 +0,0 @@
-console.log("hi")
-alert("Nintendo copyright generator\nGenerate text in the '(C) 1996 Nintendo' style")
-const year = prompt("What year?")
-const company = prompt("What company name?")
-
-document.getElementById("year").innerHTML = `©${year} `
-document.getElementById("company").innerHTML = company
